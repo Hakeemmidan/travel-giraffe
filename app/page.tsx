45dd2ebd0eb2bac0834fffa7b5dd5924a5b37bbf
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2023-05-20 14:33:58 +0300

    Fix padding

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const features = [
 
 export default function Home() {
   return (
-    <div className="overflow-hidden h-full bg-white py-24 sm:py-32">
+    <div className="overflow-hidden h-full bg-white pt-24 pb-36">
       <div className="mx-auto max-w-7xl px-6 lg:px-8 ">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
